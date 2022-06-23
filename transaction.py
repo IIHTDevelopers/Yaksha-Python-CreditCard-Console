a0aad34b9deaa25a67b20e6c8e79b6613168b4e6
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-06-23 19:37:21 +0530

    Delete transaction.py

--- transaction.py
+++
@@ -1,6 +0,0 @@
-
-class Transaction:
-    def __init__(self,card_number,transaction_date,transaction_amount):
-        self.card_number=card_number
-        self.transaction_date=transaction_date
-        self.transaction_amount=transaction_amount
